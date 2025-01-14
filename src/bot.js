af9f54191accca2a6b2ee356dbbc8d128e0bdc33
trofimovama <trofimovama@oxagile.com>
2025-01-14 18:03:28 +0300

    Remove env file dependency

--- src/bot.js
+++ src/bot.js
@@ -1,23 +1,25 @@
-require('dotenv').config();
-
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 const webAppUrl = process.env.WEB_APP_URL;
 
-const bot = new TelegramBot(token, {polling: true});
+if (!token || !webAppUrl) {
+    throw new Error('Environment variables TELEGRAM_BOT_TOKEN and WEB_APP_URL must be set');
+}
+
+const bot = new TelegramBot(token, { polling: true });
 
 bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
 
-    if(text === '/start') {
+    if (text === '/start') {
         await bot.sendMessage(chatId, 'Flip That', {
-        reply_markup: {
-            inline_keyboard: [
-            [{text: 'Get Started', web_app: {url: webAppUrl}}]
-            ]
-        }
-        })
+            reply_markup: {
+                inline_keyboard: [
+                    [{ text: 'Get Started', web_app: { url: webAppUrl } }]
+                ]
+            }
+        });
     }
-})
+});
